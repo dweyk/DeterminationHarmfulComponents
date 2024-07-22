0e2077430720e042b814e6aa88cad2fc2bbc9869
Vitaliy Murtazin <MurtazinV@terralink.ru>
2024-07-22 17:21:25 +0300

    init

---
+++ Services/Services/CompositionInformationService.cs
@@ -0,0 +1,67 @@
+﻿namespace Services;
+
+using System.Collections;
+
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+using SerpApi;
+using Contracts;
+
+/// <inheritdoc/>
+public class CompositionInformationService : ICompositionInformationService
+{
+    private readonly ILogger<PhotoEditingService> _logger;
+    private readonly IConfiguration _configuration;
+
+    public CompositionInformationService(ILogger<PhotoEditingService> logger,
+                                         IConfiguration configuration)
+    {
+        _logger = logger;
+        _configuration = configuration;
+    }
+
+    /// <inheritdoc/>
+    public async Task<string> GetCompositionInformationAsync(IEnumerable<string> searchesText)
+    {
+        var config = _configuration.GetSection("Settings");
+
+        var apiKey = config["ApiKey"] ?? throw new ArgumentNullException("Не установлен ключ для Azure Bing Api");
+
+        var listResult = new List<string>();
+
+        var task = Parallel.ForEachAsync(searchesText, (searchText, token) =>
+        {
+            var hashtable = new Hashtable
+            {
+                {"q", searchText},
+                {"hl", "ru"},
+                {"google_domain", "google.com"},
+            };
+
+            var search = new GoogleSearch(hashtable, apiKey);
+
+            if (search != null!)
+            {
+                var jsonObject = search.GetJson();
+            
+                var results = jsonObject["related_questions"]?.Children().ToList();
+
+                if (results != null)
+                {
+                    var data = results[0].ToObject<RelatedQuestionDto>();
+                
+                    listResult.Add(data?.Snippet);
+                }
+            }
+            
+            return default;
+        });
+
+        await task.WaitAsync(new CancellationToken());
+
+        var result = string.Join($"{Environment.NewLine}{Environment.NewLine}", listResult) + ".";
+
+        return result;
+    }
+}
---
+++ host/CustomSetting.cs
@@ -0,0 +1,31 @@
+﻿namespace PhotoEditingService;
+
+/// <summary>
+///     Методы расширения ConfigureHostBuilder
+/// </summary>
+public static class CustomSetting
+{
+    /// <summary>
+    ///     Использовать кастомный конфиг для приложения
+    /// </summary>
+    /// <param name="host">
+    ///     Хост
+    /// </param>
+    public static void SetCustomSettings(this ConfigureHostBuilder host)
+    {
+        host.ConfigureAppConfiguration((context, configurationBuilder)
+            => {
+                configurationBuilder.AddJsonFile("custom.settings.json",
+                    optional: true,
+                    reloadOnChange: true);
+
+                throw new VitaliyException();
+            }
+            );
+    }
+}
+
+public class VitaliyException : Exception
+{
+    public override string Message => "Если ты ни на чем не стоишь, то ты упадешь бля от всего!";
+}
